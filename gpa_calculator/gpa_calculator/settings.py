30c281b393a1d8e9b32bfb2bba96999db80a879c
ik04 <ishaankhurana123@gmail.com>
2024-02-12 14:59:36 +0530

    feat: update Allowed_hosts

--- gpa_calculator/gpa_calculator/settings.py
+++ gpa_calculator/gpa_calculator/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-eyueu^tr@k+aboe!6gpd%#*+=ur!8b4xh$k-vglo3jkl$fc**v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["gpa-calculator-h913.onrender.comz"]
+ALLOWED_HOSTS = ['gpa-calculator-h913.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
